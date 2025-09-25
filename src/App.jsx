20cfea2476dc4fd8b50d7e5c8f1bd35676b94428
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2025-09-26 03:54:53 +0530

    Redux store configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Footer, Header } from "./components/index.js";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        authService.getCurrentUser()
+        .then((userData) => {
+            if (userData) {
+                dispatch(login({userData}));
+            } else {
+                dispatch(logout())
+            }
+        })
+        .finally(() => setLoading(false));
+    }, [])
+
+    return !loading ? (
+        <div>
+            <Header />
+            <main>
+                TODO : <Outlet />
+            </main>
+            <Footer />
+        </div>
+    ) : null;
 }
 
-export default App
+export default App;
